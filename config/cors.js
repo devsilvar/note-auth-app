135f3d9f47e20ef489a7281bf500b3ef730c777a
refsilva@yahoo.com <refsilva@yahoo.com>
2026-07-12 02:21:19 +0100

    addde cors link

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
-    "http://localhost:5174",
     "http://localhost:5173",
-    "http://www.frontend.com"
+     process.env.FRONTEND_URL,
 ].filter(Boolean)
 
 const corsOption = {
